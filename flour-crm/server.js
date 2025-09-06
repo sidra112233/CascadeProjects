e26c97d0938d6ec6fd7e05c02f45ce97551a96f5
Sidra Yasmeen <sidrayasmeen081@gmail.com>
2025-09-06 15:37:23 +0500

    commit

--- flour-crm/server.js
+++ flour-crm/server.js
@@ -20,7 +20,6 @@ async function testConnection() {
 testConnection();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
